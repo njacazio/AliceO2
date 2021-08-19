2d265c33efa2ab4f96a33e071f0f0d2888862668
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2021-08-19 10:25:27 +0200

    Remove events from process

--- Analysis/ALICE3/src/alice3-lutmaker.cxx
+++ Analysis/ALICE3/src/alice3-lutmaker.cxx
@@ -185,7 +185,7 @@ struct Alice3LutMaker {
     histos.add("QA/CovMat_c1Pt21Pt2", "c1Pt21Pt2" + commonTitle, kTH3F, {axisPt, axisEta, axisc1Pt21Pt2});
   }
 
-  void process(const soa::Join<o2::aod::Collisions, o2::aod::McCollisionLabels>::iterator& coll,
+  void process(//const soa::Join<o2::aod::Collisions, o2::aod::McCollisionLabels>::iterator& coll,
                const soa::Join<aod::Tracks, aod::TracksCov, aod::McTrackLabels>& tracks,
                const aod::McParticles& mcParticles, const o2::aod::McCollisions&)
   {
