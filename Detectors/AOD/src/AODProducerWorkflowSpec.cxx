b5ce66ec2043ec8450b1df6ec19cbcd1e3484036
Jan Fiete <jgrosseo@cern.ch>
2021-09-16 13:49:02 +0200

    Change of Track and FwdTrack Data Model

--- Detectors/AOD/src/AODProducerWorkflowSpec.cxx
+++ Detectors/AOD/src/AODProducerWorkflowSpec.cxx
@@ -249,13 +249,12 @@ void AODProducerWorkflowDPL::fillTrackTablesPerCollision(int collisionID,
               math_utils::Point3D<double> vertex{};
               // FIXME: should we get better
               // than {0,0,0} as vertex here ?
-              addToFwdTracksTable(fwdTracksCursor, track, -1, meanIR.toLong(), vertex);
+              addToFwdTracksTable(fwdTracksCursor, track, -1, vertex);
             }
           } else {
-            long bcID{-1}; // FIXME: how do I get bcID ?
             math_utils::Point3D<double> vtx{vertex.getX(),
                                             vertex.getY(), vertex.getZ()};
-            addToFwdTracksTable(fwdTracksCursor, track, collisionID, bcID, vtx);
+            addToFwdTracksTable(fwdTracksCursor, track, collisionID, vtx);
           }
         } else {
           auto contributorsGID = data.getSingleDetectorRefs(trackIndex);
@@ -318,7 +317,6 @@ void AODProducerWorkflowDPL::fillTrackTablesPerCollision(int collisionID,
 template <typename FwdTracksCursorType>
 void AODProducerWorkflowDPL::addToFwdTracksTable(FwdTracksCursorType& fwdTracksCursor,
                                                  const o2::mch::TrackMCH& track, int collisionID,
-                                                 int bcID,
                                                  const math_utils::Point3D<double>& vertex)
 
 {
@@ -380,7 +378,6 @@ void AODProducerWorkflowDPL::addToFwdTracksTable(FwdTracksCursorType& fwdTracksC
 
   fwdTracksCursor(0,
                   collisionID,
-                  bcID,
                   o2::aod::fwdtrack::MCHStandaloneTrack,
                   x,
                   y,
@@ -396,7 +393,9 @@ void AODProducerWorkflowDPL::addToFwdTracksTable(FwdTracksCursorType& fwdTracksC
                   -1.0, // chi2matchmchmft,
                   -1.0, // matchscoremchmft,
                   -1,   // matchmfttrackid,
-                  -1    // matchmchtrackid
+                  -1,   // matchmchtrackid
+                  -1.f, // trackTime
+                  -1.f  // trackTimeRes
   );
 }
 
