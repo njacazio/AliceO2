8d13e406c6702be7a764d5414471806f1f7a7a57
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2022-11-08 14:32:08 +0100

    Add executable

--- DataFormats/Detectors/TOF/src/ParameterContainers.cxx
+++ DataFormats/Detectors/TOF/src/ParameterContainers.cxx
@@ -11,8 +11,29 @@
 
 /// \file   ParameterContainers.h
 /// \author Nicolò Jacazio nicolo.jacazio@cern.ch
+/// @since  2022-11-08
 /// \brief  Implementation of the containers for the general parameters
 
 #include "DataFormatsTOF/ParameterContainers.h"
 
 // ClassImp(o2::tof::Parameters);
+using namespace o2::tof;
+
+template <int nPar>
+bool ParameterCollection::LoadParameters(Parameters<nPar>& p, const std::string& key) const
+{
+  if (mParameters.find(key) == mParameters.end()) { // Can't find the required key
+    return false;
+  }
+
+  const auto& toGet = mParameters.at(key);
+  for (int i = 0; i < p.size(); i++) {
+    const auto& name = p.GetParameterName(i);
+    if (toGet.find(name) == toGet.end()) {
+      LOG(debug) << "Did not find parameter " << name << " in collection, keeping preexisting";
+      continue;
+    }
+    p.SetParameter(i, toGet.at(name));
+  }
+  return true;
+}
